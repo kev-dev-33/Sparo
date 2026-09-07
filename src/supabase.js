b2cec190468e3bd486e68024866d80cd5c4680f9
Kévin Pascaud <keke@macbook-pro-de-kevin1.home>
2026-09-07 19:15:35 +0200

    fix supabase url typo

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mliwoegmbhtkzgijawt.supabase.co'
+const supabaseUrl = 'https://mliwoegmbhtkzgijjawt.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1saXdvZWdtYmh0a3pnaWphd3QiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc1NzI0NTUyNywiZXhwIjoyMDcyODIxNTI3fQ.bd6wXHpKn6OyCcY50MNBo11SJ4Y2nrd7QIvtM0qGdRI'
 
 export const supabase = createClient(
