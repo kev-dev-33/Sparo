d9afb55a81eb4c2286c8c51fffc8f62742b9ffdb
Kévin Pascaud <keke@macbook-pro-de-kevin1.home>
2026-09-07 18:17:57 +0200

    Hardcode Supabase keys

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY
+const supabaseUrl = 'https://mliwoegmbhtkzgijawt.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1saXdvZWdtYmh0a3pnaWphd3QiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc1NzI0NTUyNywiZXhwIjoyMDcyODIxNTI3fQ.bd6wXHpKn6OyCcY50MNBo11SJ4Y2nrd7QIvtM0qGdRI'
 
 export const supabase = createClient(
   supabaseUrl,
