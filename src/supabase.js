2469e53e828dd72f1f6696ae1fff43c2e220b7c4
Kévin Pascaud <keke@macbook-pro-de-kevin1.home>
2026-09-08 15:53:11 +0200

    fix: use env variables instead of hardcoded supabase keys

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mliwoegmbhtkzgijjawt.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1saXdvZWdtYmh0a3pnaWphd3QiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc1NzI0NTUyNywiZXhwIjoyMDcyODIxNTI3fQ.bd6wXHpKn6OyCcY50MNBo11SJ4Y2nrd7QIvtM0qGdRI'
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey,
